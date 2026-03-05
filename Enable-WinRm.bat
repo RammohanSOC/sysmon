8dbf404faab626a769b5dc67614963d4220bcc43
RammohanSOC <abburirammohan2@gmail.com>
2026-03-05 10:18:48 +0530

    Create Enable-WinRm.bat

---
+++ Enable-WinRm.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo =========================================
+echo Configuring Windows for Ansible WinRM
+echo =========================================
+echo Enabling WinRM service...
+winrm quickconfig -q
+
+echo Enabling PowerShell Remoting...
+powershell -Command "Enable-PSRemoting -Force"
+
+echo Enabling Basic Authentication...
+winrm set winrm/config/service/auth @{Basic="true"}
+
+echo Allowing unencrypted WinRM traffic...
+winrm set winrm/config/service @{AllowUnencrypted="true"}
+
+echo Opening Firewall Port 5985...
+netsh advfirewall firewall add rule name="WinRM-5985" dir=in action=allow protocol=TCP localport=5985
+
+echo Creating Ansible user...
+net user ansible P@ssw0rd123 /add
+
+echo Adding user to Administrators group...
+net localgroup administrators ansible /add
+
+echo =========================================
+echo WinRM Configuration Completed
+echo =========================================
+
+pause
